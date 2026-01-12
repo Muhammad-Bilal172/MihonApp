04e93aa9c8484cca112e90511abd24a904c1ef66
Muhammad Bilal <muhammadbilal@Muhammads-MacBook-Pro-2.local>
2026-01-12 21:16:48 +0500

    Again Changed

--- MangaScrap/library.py
+++ MangaScrap/library.py
@@ -21,7 +21,7 @@ def library_func(app):
         if categories is None:
             cursor.execute("INSERT INTO library_categories (user_id, category_name) VALUES (%s, %s)", (user_id, "Default Library"))
 
-        theme = "dark"
+        theme = request.cookies.get("theme")
 
         cursor.execute("SELECT downloaded_only_mode FROM download_only WHERE user_id = %s", (user_id,))
         download_only_mode = cursor.fetchone()
